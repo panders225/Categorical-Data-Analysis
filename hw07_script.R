c1596cd1ba62dfa2ad8a8db22140f33942c306b0
Philip Anderson <panders225@gmail.com>
2017-07-16 21:04:48 -0400

    finished hw07

--- hw07_script.R
+++ hw07_script.R
@@ -126,5 +126,147 @@ summary(soc_mod2)
 exp(soc_mod2$coefficients) %>% reshape2::melt()
 
 
+# 7.24
+# input the data
+relig <- read.csv("C:/Users/Philip/Schools/TAMU/STAT_659/homework/git_hw/relig.csv")
+names(relig) <- tolower(names(relig))
+relig$ra <- as.factor(relig$ra)
+relig$tbc <- as.factor(relig$tbc)
 
+#A. fit the independence model
+relig_mod <- glm(
+  formula=count ~ ra + tbc
+  , data=relig
+  , family=poisson(link="log")
+                )
+summary(relig_mod)
 
+subset(relig_mod$residuals, abs(relig_mod$residuals) > 2)
+relig[32,]
+plot(density(relig_mod$residuals))
+1 - pchisq(112.54, df=24)
+# prepare the data for the linear-by-linear association model
+# these need to remain numbers for ordinality - cannot be factors
+relig$mu <- as.integer(relig$ra)
+relig$nu <- as.integer(relig$tbc)
+
+# fit the linear-by-linear association model
+relig_mod2 <- glm(
+  formula=count~ra+tbc+mu:nu
+  , data=relig
+  , family=poisson(link="log")
+)
+
+summary(relig_mod2)
+
+1 - pchisq(19.901, df=23)
+
+# get new column scores
+relig$nu_rescore <- rep(c(1,2,4,5),9)
+relig
+
+# refit the model
+relig_mod3 <- glm(
+  formula=count~ra+tbc+mu:nu_rescore
+  , data=relig
+  , family=poisson(link="log")
+)
+
+summary(relig_mod3)
+# does not appear to have improved the model
+
+anova(relig_mod2, relig_mod3)
+
+# now, fit the row effects model
+relig_mod4 <- glm(
+  formula=count~ra + tbc + ra:nu
+  , data=relig
+  , family=poisson(link="log")
+  )
+summary(relig_mod4)
+# now, fit the column effects model
+
+relig_mod5 <- glm(
+  formula=count~ra + tbc + tbc:mu
+  , data=relig
+  , family=poisson(link="log")
+  )
+summary(relig_mod5)
+
+# fit the row and column effect model
+relig_mod6 <- glm(
+  formula=count~ra + tbc + ra:nu + tbc:mu
+  , data=relig
+  , family=poisson(link="log")
+  )
+summary(relig_mod6)
+
+# saturated model
+relig_mod7 <- glm(
+  formula= count~(ra*tbc*mu*nu)
+  , data=relig
+  , family=poisson(link="log")
+)
+summary(relig_mod7)
+
+# deviance table 
+#independence model
+deviance(relig_mod)
+summary(relig_mod)
+AIC(relig_mod)
+#linear by linear model
+deviance(relig_mod3)
+summary(relig_mod3)
+AIC(relig_mod2)
+#row effect
+deviance(relig_mod4)
+summary(relig_mod4)
+AIC(relig_mod4)
+
+#column effect
+deviance(relig_mod5)
+summary(relig_mod5)
+AIC(relig_mod5)
+
+#row and column effect
+deviance(relig_mod6)
+summary(relig_mod6)
+AIC(relig_mod6)
+
+#saturated model
+deviance(relig_mod7)
+summary(relig_mod7)
+AIC()
+1 - pchisq(10.92, df=14)
+
+# Additional Problem 1
+# input the data
+
+gender <- c(rep("M",4), rep("F",4))
+country <- rep(c(rep("USA",2), rep("UK",2)),2)
+switch <- rep(c("Y", "N", "Y", "N"),2)
+count <- c(29,6,19,15,7,23,24,29)
+soft <- data.frame(gender=gender, country=country, switch=switch, count=count)
+
+modln <- function(mod)
+  {
+soft_mod <- glm(
+  formula=mod
+                , data=soft
+                , family=poisson(link="log")
+                )
+ print(AIC(soft_mod))
+  }
+
+modln(count ~gender + country + switch)
+modln(count ~gender + country + switch + gender:country)
+modln(count ~gender + country + switch + gender:country + country:switch)
+modln(count ~gender + country + switch + gender:country + country:switch + gender:switch)
+
+soft_mod <- glm(
+  formula=count ~gender + country + switch + gender:country + country:switch + gender:switch
+                , data=soft
+                , family=poisson(link="log")
+                )
+summary(soft_mod)
+ 1 - pchisq(10.138, df=1)
